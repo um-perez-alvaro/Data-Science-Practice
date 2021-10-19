98ad8a2870c17244bafd8574c0434051ef129d8d
um-perez-alvaro <javier.perez-alvaro@mso.umt.edu>
2021-10-19 09:46:43 -0600

    clf trees pps

--- Jupyter Notebooks/Classification/notebooks/functions.py
+++ Jupyter Notebooks/Classification/notebooks/functions.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Sep 16 14:29:00 2021
+Updated on Tue Oct 19 09:00:00 2021
 
 @author: javier.perez-alvaro
 """
@@ -8,9 +9,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 def draw_classification_regions(X,y,model):
     
+    # classes 
+    classes = model.classes_
+    # feature names
+    try:
+        feature_names = X.columns
+    except:
+        feature_names = ['feature #1','feature #2']
+    
+    # put data into numpy arrays
+    try:
+        X = np.array(X)
+        y = np.array(y).flatten()
+    except:
+        print('something went wrong')
+    
     from matplotlib.colors import ListedColormap
     # create a mesh grid
-    h = .1
+    h = .01
     x_min, x_max = X[:, 0].min() - .1, X[:, 0].max() + .1
     y_min, y_max = X[:, 1].min() - .1, X[:, 1].max() + .1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
@@ -22,6 +38,10 @@ def draw_classification_regions(X,y,model):
     
     # number of classes
     n_classes = len(np.unique(y))
+    
+    # map classes to 0,1,2
+    for i in range(n_classes):
+        Z[Z==classes[i]]=i
 
     # Put the result into a color plot
     fig, ax = plt.subplots(figsize=(10,5))
@@ -35,9 +55,9 @@ def draw_classification_regions(X,y,model):
 
     # plot dataset
     for i in range(n_classes):
-        ax.scatter(X[y==i,0], X[y==i,1], c = colors[i], label='class '+str(i), s=75, alpha=0.8)
+        ax.scatter(X[y==classes[i],0], X[y==classes[i],1], c = colors[i], label=classes[i], s=75, alpha=0.8)
 
-    ax.set_xlabel('feature #1', fontsize=15)
-    ax.set_ylabel('feature #2', fontsize=15)
+    ax.set_xlabel(feature_names[0], fontsize=15)
+    ax.set_ylabel(feature_names[1], fontsize=15)
 
     ax.legend()
